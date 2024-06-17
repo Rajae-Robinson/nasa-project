fcd1a8b2369afbe1a6f48c0f286643e93a92a31c
Rajae Robinson <rajrobinson18@gmail.com>
2024-06-16 21:40:34 -0500

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,8 @@ const v1API = require('./routes/v1')
 
 const app = express()
 
+app.set('trust proxy', 1)
+
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
 	limit: 100, 
@@ -32,6 +34,8 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/v1', v1API)
+
+app.get('/x-forwarded-for', (request, response) => response.send(request.headers['x-forwarded-for']))
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
