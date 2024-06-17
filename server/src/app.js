f4c40492b113bc3b9698417c86cd3fdc07d7ec66
Rajae Robinson <rajrobinson18@gmail.com>
2024-06-16 20:45:45 -0500

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -9,8 +9,6 @@ const v1API = require('./routes/v1')
 
 const app = express()
 
-app.set('trust proxy', 1);
-
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
 	limit: 100, 
@@ -22,8 +20,7 @@ const limiter = rateLimit({
 // app.use(helmet())
 
 app.use(cors({
-    origin: 'http://localhost:3000',
-	exposedHeaders: ['RateLimit-Limit', 'RateLimit-Remaining', 'RateLimit-Reset']
+    origin: 'http://localhost:3000'
 }))
 
 app.use(limiter)
