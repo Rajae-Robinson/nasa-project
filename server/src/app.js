2aeca7dce4c0983d66809760c72320c602211441
Rajae Robinson <rajrobinson18@gmail.com>
2024-06-16 22:43:30 -0500

    log ips

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const v1API = require('./routes/v1')
 
 const app = express()
 
-app.set('trust proxy', 1);
+app.set('trust proxy', true);
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
@@ -34,6 +34,11 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/v1', v1API)
+
+app.use((req, res, next) => {
+    console.log(`Request IP: ${req.ip}`);
+    next();
+});
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
