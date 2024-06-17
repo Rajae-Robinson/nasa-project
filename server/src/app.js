31b25ef2e59509d6666cc27197aed987f62f9651
Rajae Robinson <rajrobinson18@gmail.com>
2024-06-17 09:02:41 -0500

    chore(server): remove debug endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -12,8 +12,6 @@ const v1API = require('./routes/v1')
 
 const app = express()
 
-app.set('trust proxy', 1);
-
 var accessLogStream = rfs.createStream('access.log', {
     size: '10M',
     interval: '1d', 
@@ -37,17 +35,14 @@ app.use(cors({
 app.use(limiter)
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+    app.use(morgan('dev'))
 } else {
-    app.use(morgan('combined', { stream: accessLogStream }));
+    app.use(morgan('combined', { stream: accessLogStream }))
 }
 
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../public')))
-
-app.get('/ip', (request, response) => response.send(request.ip))
-app.get('/x-forwarded-for', (request, response) => response.send(request.headers['x-forwarded-for']))
 
 app.use('/v1', v1API)
 
