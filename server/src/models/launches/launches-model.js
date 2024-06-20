0e571ab1aaefd92daf281fa1c36f2f014ccf9100
Rajae Robinson <rajrobinson18@gmail.com>
2024-06-20 06:46:05 -0500

    refactor(server): separate logic for calling spacex api

--- server/src/models/launches/launches-model.js
+++ server/src/models/launches/launches-model.js
@@ -1,59 +1,9 @@
-const axios = require('axios')
-
 const launches = require('./launches-schema')
 const planets = require('../planets/planets-schema');
+const { populateLaunches } = require('../../services/tesla-launch-data-service');
 const { logger } = require('../../utils/logger');
 
 DEFAULT_FLIGHT_NUMBER = 100
-
-const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
-
-async function populateLaunches() {
-    const response = await axios.post(SPACEX_API_URL, {
-      query: {},
-      options: {
-        pagination: false,
-        populate: [
-          {
-            path: 'rocket',
-            select: {
-              name: 1
-            }
-          },
-          {
-            path: 'payloads',
-            select: {
-              'customers': 1
-            }
-          }
-        ]
-      }
-    })
-  
-    if (response.status !== 200) {
-      throw new Error('Launch data download failed');
-    }
-  
-    const launchDocs = response.data.docs;
-    for (const launchDoc of launchDocs) {
-      const payloads = launchDoc['payloads'];
-      const customers = payloads.flatMap((payload) => {
-        return payload['customers'];
-      });
-  
-      const launch = {
-        flightNumber: launchDoc['flight_number'],
-        mission: launchDoc['name'],
-        rocket: launchDoc['rocket']['name'],
-        launchDate: launchDoc['date_local'],
-        upcoming: launchDoc['upcoming'],
-        success: launchDoc['success'],
-        customers,
-      };
-  
-      await saveLaunch(launch);
-    }
-}
 
 async function findLaunch(filter) {
     return await launches.findOne(filter);
