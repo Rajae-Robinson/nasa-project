e37c161b458ebfd48db00c4450139191bdf2a676
Rajae Robinson <rajrobinson18@gmail.com>
2024-06-22 14:49:10 -0500

    feat(server): create user on sign-up

--- server/src/models/user/user-model.js
+++ server/src/models/user/user-model.js
@@ -1,7 +1,11 @@
 const User = require("./user-schema");
 
 async function createUser({name, email, password, passwordConfirm}) {
-    await User.create({name, email, password, passwordConfirm})
+    try {
+        return await User.create({name, email, password, passwordConfirm})
+    } catch(err) {
+        throw err
+    }
 }
 
 module.exports = {
--- server/src/routes/auth/auth-controller.js
+++ server/src/routes/auth/auth-controller.js
@@ -1,22 +1,26 @@
 const { createUser } = require("../../models/user/user-model");
 const catchAsync = require("../../utils/catch-async");
 
-async function signUp(req, res) {
-    // const newUser = createUser({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    //     passwordConfirm: req.body.passwordConfirm
-    // })
-
-    // sanitize and validate
-
-    res.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser
-        }
-    })
+async function signUp(req, res, next) {
+    try {
+        const newUser = await createUser({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm
+        })
+    
+        // sanitize and validate
+    
+        res.status(201).json({
+            status: 'success',
+            data: {
+                user: newUser
+            }
+        })
+    } catch(err) {
+        next(err)
+    }
 }
 
 module.exports = {
